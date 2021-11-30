8ace01e2e1ea44daf4192c8e2f190d6b2d70da73
Vanesa Kanneman <vanesaledesma90@gmail.com>
2021-11-30 13:29:35 -0300

    Cambios pagina ver mas  y home

--- PORTFOLIO_VASQUEZ/frontend/src/pages/HomePage.js
+++ PORTFOLIO_VASQUEZ/frontend/src/pages/HomePage.js
@@ -21,24 +21,86 @@ const HomePage = (props)=>{
                 <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
                   <div className="carousel-inner">
                         <div className="carousel-item active" data-bs-interval="2000">
-                        <img src="img/vertico 1.png" className="d-block w-100" alt="..."/>
+                        <img src="img/bototipicu 1.png" className="d-block w-100" alt="..."/>
                         </div>
                         <div className="carousel-item" data-bs-interval="2000">
-                        <img src="img/vertico 2.png" className="d-block w-100" alt="..."/>
+                        <img src="img/bototipicu 2.png" className="d-block w-100" alt="..."/>
                         </div>
                         <div className="carousel-item" data-bs-interval="2000">
-                        <img src="img/vertico 3.png" className="d-block w-100" alt="..."/>
+                        <img src="img/bototipicu 3.png" className="d-block w-100" alt="..."/>
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                        <img src="img/bototipicu 4.png" className="d-block w-100" alt="..."/>
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                        <img src="img/bototipicu 5.png" className="d-block w-100" alt="..."/>
                         </div>
                   </div>
                 </div>
               </div>
               
             </div>
+          </div>
+          <div className="container">
+          <div className="row skill">
+            <h3 className="titulo3"> Programas - Framework - Tecnologias y CMR que más uso</h3><br/>
+              <div className="col">
+                <div className="thumbnail">            
+                    <img clasName="" src="img/illustrator.png" alt="Illustrator" width="50%" />
+                    <div className="caption">
+                      <p>Illustrator</p>
+                    </div>            
+                </div>
+              </div>
 
-          
+              <div className="col">
+                <div className="thumbnail">            
+                    <img className="" src="img/bootstrap.png" alt="Bootstrap" width="50%"/>
+                    <div className="caption">
+                      <p>Bootstrap</p>
+                    </div>            
+                </div>
+              </div>
 
+              <div className="col">
+                <div className="thumbnail">            
+                    <img className="" src="img/html-5.png" alt="HTML 5" width="50%" />
+                    <div className="caption">
+                      <p>HTML5</p>
+                    </div>            
+                </div>
+              </div>
+
+              <div className="col">
+                <div className="thumbnail">            
+                    <img className="" src="img/css-3.png" alt="CSS3" width="50%" />
+                    <div className="caption">
+                      <p>CSS3</p>
+                    </div>            
+                </div>
+              </div>
+
+              <div className="col">
+                <div className="thumbnail">            
+                    <img className="" src="img/javascript.png" alt="JavaScript" width="50%"/>
+                    <div className="caption">
+                      <p>JS</p>
+                    </div>            
+                </div>
+              </div>
+
+              <div className="col">
+                <div className="thumbnail">            
+                    <img className="img-responsive" src="img/wordpress.png" alt="Wordpress" width="50%"/>
+                    <div className="caption">
+                      <p>Wordpress</p>
+                    </div>            
+                </div>
+              </div>
+
+            </div>
+          </div>
           
-        </div>
         
       </main>
     );

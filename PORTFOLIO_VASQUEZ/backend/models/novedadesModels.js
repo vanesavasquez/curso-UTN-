11aa1cb09e92a62d33d5a28d9bf32faeb83d6902
Vanesa Kanneman <vanesaledesma90@gmail.com>
2021-12-07 13:22:28 -0300

    modificar las entradas desde el admin M6

--- PORTFOLIO_VASQUEZ/backend/models/novedadesModels.js
+++ PORTFOLIO_VASQUEZ/backend/models/novedadesModels.js
@@ -23,4 +23,25 @@ async function deleteNovedadById(id) {
     var rows = await pool.query(query, [id]);
     return rows;
 }
-module.exports= {getnovedades,insertNovedades, deleteNovedadById}
+
+//edicion
+// funcion q trae la entrada a editar por el id
+async function getNovedadById(id) {
+    var query = "select * from novedades where id = ? ";
+    var rows = await pool.query(query, [id]);
+    return rows[0];
+}
+
+//editar la entrada seleccionada
+
+async function modificarNovedadById(obj, id) {
+    try {
+        var query = "update novedades set ? where id=?";
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports= {getnovedades,insertNovedades, deleteNovedadById, modificarNovedadById, getNovedadById}
--- PORTFOLIO_VASQUEZ/backend/routes/admin/blog.js
+++ PORTFOLIO_VASQUEZ/backend/routes/admin/blog.js
@@ -55,4 +55,38 @@ router.get('/eliminar/:id', async (req, res, next) => {
     await novedadesModel.deleteNovedadById(id);
     res.redirect('/admin/blog');
 });
+
+//traer la entrada a editar y enviar a la vista q la va a modificar
+
+router.get('/editar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    //console.log(req.params.id);//para ver si lo esta guardando
+    var novedad = await novedadesModel.getNovedadById(id);
+    res.render('admin/editar', {
+        layout: 'admin/layout',
+        novedad,
+    });
+});
+
+//ahra vamso a modifiar la entrada que seleccionamos para editar, con los datos nuevos y los vamos a cargar en la bd
+router.post('/modificar', async (req, res, next) => {
+    try {
+        var obj={
+            titulo: req.body.titulo,
+            subtitulo: req.body.subtitulo,
+            cuerpo: req.body.cuerpo
+        }
+        console.log(obj);//para ver si trae los datos nuevos
+        await novedadesModel.modificarNovedadById(obj, req.body.id);
+        res.redirect('/admin/blog');
+    } catch (error) {
+        console.log(error);
+        res.render('admin/editar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se pudo modificar la novedad',
+        });
+    }
+});
+
 module.exports=router;
